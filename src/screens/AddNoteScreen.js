13f5cb6c3291b238438cb273740cb64c629e4328
matthewberzinskas <pasomatt@gmail.com>
2022-05-11 14:51:27 -0700

    Added multi-line text editor for new note

--- src/screens/AddNoteScreen.js
+++ src/screens/AddNoteScreen.js
@@ -1,9 +1,30 @@
-import { Text, View } from "react-native";
+import React, { useState } from "react";
+import { Text, View, TextInput, StyleSheet } from "react-native";
 
 export default function AddNoteScreen() {
+  const [text, onChangeText] = useState("");
+  const [height, setHeight] = useState(40);
+
   return (
     <View>
       <Text>AddNote Placeholder</Text>
+      <TextInput
+        placeholder="Your Placeholder"
+        onChangeText={onChangeText}
+        style={[styles.input, { height: height }]}
+        editable
+        multiline
+        value={text}
+        onContentSizeChange={(e) => setHeight(e.nativeEvent.contentSize.height)}
+      />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  input: {
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  },
+});
